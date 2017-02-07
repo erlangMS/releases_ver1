6b85421f98e7a12f29465e6912997c623ec2806d
evertonagilar <evertonagilar@gmail.com>
2017-02-07 14:02:47 -0500

    setup centos

--- setup/setup_emsbus-1.0.11-el7.centos.x86_64.sh
+++ setup/setup_emsbus-1.0.11-el7.centos.x86_64.sh
@@ -27,24 +27,71 @@
 #
 ########################################################################################################
 
-LINUX_DESCRIPTION=$(lsb_release -d | sed -rn 's/Description:\t(.*$)/\1/p')
+
+# ***** Parameters to script *****
+#LINUX_DESCRIPTION=$(lsb_release -d 2> /dev/null | sed -rn 's/Description:\t(.*$)/\1/p')
+VERSION_SETUP="ems-bus-1.0.11-el7.centos.x86_64"
+LINUX_DESCRIPTION=$(cat /etc/redhat-release 2> /dev/null)
+YUM_UPDATE_NECESSARY="false"
+CURRENT_DIR=$(pwd)
+TMP_DIR="/tmp/setup_$VERSION_SETUP_$$/"
+
+
+# Enables installation logging
+LOG_FILE="setup_""$VERSION_SETUP""_$(date '+%d%m%Y_%H%M%S').log"
+exec > >(tee -a ${LOG_FILE} )
+exec 2> >(tee -a ${LOG_FILE} >&2)
+
+
+# Files downloaded go to /setup_emsbus_$$
+mkdir -p $TMP_DIR && cd $TMP_DIR
+
 
 echo "Starting the ERLANGMS installation on $LINUX_DESCRIPTION"
+echo "Purpose: A service-oriented bus developed in Erlang/OTP by Everton de Vargas Agilar"
+echo "Version: $VERSION_SETUP"
+echo "Log file: $LOG_FILE" 
+echo "Date: $(date '+%d/%m/%Y %H:%M:%S')"
 echo "============================================================================="
+
+
+# ***** EPEL 7 repository **********
+echo "Installing the latest EPEL 7 repository..."
+if ! rpm -qi epel-release  >> /dev/null ; then
+	sudo yum -y install epel-release
+	YUM_UPDATE_NECESSARY="true"
+else
+	echo "Skipping EPEL 7 repository installation because it is already installed."
+fi
+
 
 
 # ***** Erlang Runtime Library **********
 
+# erlang-solutions is a rpm package for Erlang repository
 if ! rpm -qi erlang-solutions >> /dev/null ; then
 	echo "Adding Erlang repository entry"
-	wget https://packages.erlang-solutions.com/erlang-solutions-1.0-1.noarch.rpm
-	sudo rpm -Uvh erlang-solutions-1.0-1.noarch.rpm
+	wget -nv https://packages.erlang-solutions.com/erlang-solutions-1.0-1.noarch.rpm
 
-	echo "yum update..."
-	sudo apt-get update
+	# Check internet connectivity
+	if [ "$?" -eq "4" ]; then
+		echo "Make sure your DNS is well configured or if there is Internet on this host. Canceling installation..."
+		exit 1
+	fi
+
+	sudo rpm -Uvh erlang-solutions-1.0-1.noarch.rpm
+	YUM_UPDATE_NECESSARY="true"
 fi
 
 
+# update yum if necessary
+if [ "$YUM_UPDATE_NECESSARY" == "true" ]; then
+	echo "yum update..."
+	sudo yum -y update
+fi
+
+
+# Check if Erlang runtime already exist
 if ! erl -version 2> /dev/null;  then
 	echo "Installing Erlang Runtime Library packages..."
 	sudo yum -y install erlang-hipe-19.2-1.el7.centos.x86_64 \
@@ -85,27 +132,21 @@ fi
 if ! rpm -qi python34 >> /dev/null ; then
 	echo "Preparing to install python34..."
 	
-	echo "Installing yum-utils..."
 	if ! rpm -qi yum-utils  >> /dev/null ; then
+		echo "Installing yum-utils..."
 		sudo yum -y install yum-utils 
 	else
 		echo "Skipping yum-utils installation because it is already installed."
 	fi
 
-	echo "Installing the latest EPEL 7 repository..."
-	if ! rpm -qi yum-utils  >> /dev/null ; then
-		sudo yum -y install epel-release 
-	else
-		echo "Skipping EPEL 7 repository installation because it is already installed."
-	fi
-
-
 	echo "Installing python 3.4 and its libraries..."
-	sudo yum install python34 
+	sudo yum -y install python34 
 
-	echo "Installing pip..."
-	curl -O https://bootstrap.pypa.io/get-pip.py
-	sudo /usr/bin/python3.4 get-pip.py 
+	if ! pip3 --version 2> /dev/null;  then
+		echo "Installing pip..."
+		curl -O https://bootstrap.pypa.io/get-pip.py
+		sudo /usr/bin/python3.4 get-pip.py 
+	fi
 else
 	echo "Skipping python34 installation because it is already installed."
 fi
@@ -141,7 +182,7 @@ fi
 # ***** Install ems-bus *****
 if ! rpm -qi ems-bus >> /dev/null ; then
 	echo "Downloading ems-bus-1.0.11-el7.centos.x86_64.rpm..."
-	wget https://github.com/erlangMS/releases/raw/master/ems-bus_1.0.11/ems-bus-1.0.11-el7.centos.x86_64.rpm
+	wget -nv https://github.com/erlangMS/releases/raw/master/ems-bus_1.0.11/ems-bus-1.0.11-el7.centos.x86_64.rpm
 	echo "Installing ems-bus-1.0.11-el7.centos.x86_64.rpm..."
 	sudo rpm -Uhv ems-bus-1.0.11-el7.centos.x86_64.rpm
 else
@@ -149,5 +190,5 @@ else
 fi
 
 
-
-
+cd $CURRENT_DIR
+echo "Ok!!!"
